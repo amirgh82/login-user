d6ff69c802470e54f02fcfa3740ffca163233b08
amirgh82 <amirghhastam@gmail.com>
2023-10-01 12:40:15 +0330

    write login conatiner

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -3,6 +3,15 @@ import './Login.css'
 
 export default function Login() {
   return (
-    <div>Login</div>
+    <>
+      <div className="login-container">
+        <div className="login-form">
+          <form>
+            <h2 className="login-title">صفحه ورود کاربر</h2>
+          </form>
+        </div>
+        <div className="login-picture"></div>
+      </div>
+    </>
   )
 }
