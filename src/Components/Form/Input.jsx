f88f554b680e9b8334bae197b19d3f66a96147d9
amirgh82 <amirghhastam@gmail.com>
2023-10-01 20:02:14 +0330

    fix bug input and add icon in input

--- src/Components/Form/Input.jsx
+++ src/Components/Form/Input.jsx
@@ -2,12 +2,13 @@ import React, { useRef } from 'react'
 import './Input.css'
 
 
-export default function Input({ type, placeholder }) {
+export default function Input({ type, placeholder, icon }) {
 
     return (
         <div className="form-control">
+            <span>{icon}</span>
             <input type={type} className='form-input' required />
-            <span>{placeholder}</span>
+            <label>{placeholder}</label>
         </div>
     )
 }
--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import './Login.css'
 import Input from '../../Components/Form/Input'
+import { FaRegUserCircle } from 'react-icons/fa'
+import { AiOutlineEye } from 'react-icons/ai'
+import { AiOutlineEyeInvisible } from 'react-icons/ai'
 
 export default function Login() {
   return (
@@ -9,8 +12,8 @@ export default function Login() {
         <div className="login-form">
           <h2 className="login-title">به <span>web codino </span>خوش اومدید</h2>
           <form>
-            <Input type='text' placeholder='نام کاربری یا ایمیل' />
-            <Input type='password' placeholder='رمز عبور' />
+            <Input type='text' placeholder='نام کاربری یا ایمیل' icon={<FaRegUserCircle />} />
+            <Input type='password' placeholder='رمز عبور' icon={<AiOutlineEye />} />
           </form>
         </div>
         <div className="login-picture">
