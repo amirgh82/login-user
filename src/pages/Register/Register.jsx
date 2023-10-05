7031314841930789d222d74b8527f9c55df0a933
amirgh82 <amirghhastam@gmail.com>
2023-10-05 22:40:59 +0330

    fix bug and color

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -16,13 +16,13 @@ export default function Login() {
 
     return (
         <>
-            <div className="login-container">
-                <div className="login-left">
-                    <h2 className="login-title">به <span>وب کدینو </span>خوش اومدید</h2>
-                    <img className='login-picture' src="./../../../public/images/Mobile login-amico.svg" alt="coding workshop" />
+            <div className="register-container">
+                <div className="register-right">
+                    <h2 className="register-title">به <span>وب کدینو </span>خوش اومدید</h2>
+                    <img className='register-picture' src="./../../../public/images/Mobile login-amico.svg" alt="coding workshop" />
                 </div>
-                <div className="login-form">
-                    <h2 className="login-title">ثبت نام</h2>
+                <div className="register-form">
+                    <h2 className="register-title">ثبت نام</h2>
                     <form>
                         <Input type='text' placeholder='نام کاربری' icon={<FaRegUserCircle />} />
                         <Input type='email' placeholder='ایمیل' icon={<AiOutlineMail />} />
